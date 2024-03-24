dd1fb0944078824862f5a970741de46377837714
gmauriIlerna <112699038+gmauriIlerna@users.noreply.github.com>
2024-03-24 19:53:27 +0100

    Hotfix scripts.js

--- scripts.js
+++ scripts.js
@@ -9,33 +9,29 @@ function saveToFile() {
         color: color
     };
 
-    // Headers to be sent with the request
     const headers = new Headers();
-    headers.append('Content-Type', 'application/json'); // Example header
-    headers.append('File-Name', name); // Another example header
+    headers.append('Content-Type', 'application/json');
+    headers.append('File-Name', name);
 
-    // Configuration for the fetch request
     const requestOptions = {
     method: 'POST',
     headers: headers,
-    body: JSON.stringify(data) // Convert data to JSON string
+    body: JSON.stringify(data)
     };
 
     // Making the POST request
     fetch(url, requestOptions)
     .then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.json(); // Assuming response is JSON
-    })
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+        })
     .then(data => {
-    // Handle the response data
-    console.log(data);
+        console.log(data);
     })
     .catch(error => {
-    // Handle errors
-    console.error('There was a problem with the fetch operation:', error);
+        console.error('There was a problem with the fetch operation:', error);
     });
 
     sessionStorage.setItem('file-Name', name);
@@ -48,15 +44,13 @@ function saveToFile() {
 function loadFromFile() {
     const headers = new Headers();
     headers.append('Content-Type', 'application/json');
-    headers.append('File-Name', sessionStorage.getItem('file-Name')); // Adding custom header
+    headers.append('File-Name', sessionStorage.getItem('file-Name'));
     
-    // Configuration for the fetch request
     const requestOptions = {
       method: 'GET',
       headers: headers
     };
     
-    // Making the GET request
     fetch(url, requestOptions)
     .then(response => {
         if (!response.ok) {
